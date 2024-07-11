cd1413ce498e8bbeb6592cea2fec0d1c82adfb85
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-07-11 23:06:17 +0300

    feat: load init data from LS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import './App.css';
 import { ContentSection } from './components/content-section/ContentSection';
 import { Dialog } from './components/dialog/Dialog';
 import { ErrorBoundary } from './components/error-boundarie/ErrorBoundarie';
 import { SearchSection } from './components/search-section/SearchSection';
 import { PokemonData, searchPokemonListByName } from './api/restApi';
+import {
+  getSearchValueFromLocalStorage,
+  setSearchValueToLocalStorage,
+} from './store/state';
 
 export const PokemonListContext = createContext<PokemonData[]>([]);
+
+enum SearchLimit {
+  NoLimit = '10000',
+  Limited = '100',
+}
 
 function App() {
   const [pokemonDataListState, setPokemonDataListState] = useState<
     PokemonData[]
   >([]);
 
+  function saveToLsIfSuccess(searchInput: string, pokemonData: PokemonData[]) {
+    if (searchInput && pokemonData.length) {
+      setSearchValueToLocalStorage(searchInput);
+    }
+  }
+
   async function requestPokemonData(searchInput: string) {
     setPokemonDataListState([]);
-    const pokemonData = await searchPokemonListByName(searchInput);
+    const pokemonData = await searchPokemonListByName(
+      searchInput,
+      searchInput ? SearchLimit.NoLimit : SearchLimit.Limited
+    );
+    saveToLsIfSuccess(searchInput, pokemonData);
     setPokemonDataListState(pokemonData);
   }
+
+  useEffect(() => {
+    const searchValue = getSearchValueFromLocalStorage();
+    if (searchValue) {
+      requestPokemonData(searchValue);
+      return;
+    }
+    requestPokemonData('');
+  }, []);
 
   return (
     <>
