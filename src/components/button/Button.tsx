5fce377042f635215c23e25998f2799b8ae57608
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-07-11 20:53:48 +0300

    feat: added Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -13,6 +13,7 @@ type ButtonProps = {
 export const Button = ({ title, btnType, onClick }: ButtonProps) => {
   return (
     <button
+      type="submit"
       onClick={onClick}
       className={`${style.defaultButton} ${style[btnType]}`}
     >
