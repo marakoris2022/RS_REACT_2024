be67861627b7f80f497a575824b76937be5b4f9f
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-07-11 14:53:47 +0300

    feat: added Button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,22 @@
+import style from './button.module.scss';
+
+export enum ButtonType {
+  GREEN = 'greenButton',
+}
+
+type ButtonProps = {
+  title: string;
+  btnType: ButtonType;
+  onClick: () => void;
+};
+
+export const Button = ({ title, btnType, onClick }: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${style.defaultButton} ${style[btnType]}`}
+    >
+      {title}
+    </button>
+  );
+};
