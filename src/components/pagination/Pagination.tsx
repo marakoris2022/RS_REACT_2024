8ae94d7d2c1304db588f34d63ab722ba976b952f
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-07-12 20:09:18 +0300

    feat: pagination now working with URL

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,6 +1,9 @@
+import React, { useEffect } from 'react';
 import { PokemonData } from '../../api/restApi';
 import { Button, ButtonType } from '../button/Button';
 import style from './pagination.module.scss';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { updateQueryParams } from '../../utils/utils';
 
 type PaginationProps = {
   pageNum: number;
@@ -13,25 +16,51 @@ export const Pagination = ({
   setPageNum,
   pokemonList,
 }: PaginationProps) => {
-  function calculateTotalPages(totalPokes: number) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const queryParams = new URLSearchParams(location.search);
+  let frontpage = queryParams.get('frontpage');
+
+  const calculateTotalPages = (totalPokes: number) => {
     return Math.ceil(totalPokes / 10);
-  }
+  };
 
-  function handlePageClick(num: number) {
+  const handlePageClick = (num: number) => {
     setPageNum(num);
-  }
+    updateQueryParams({ frontpage: String(num) }, navigate, location);
+  };
 
-  function handlePrevClick() {
+  const handlePrevClick = () => {
     if (pageNum > 1) {
       setPageNum((prev) => prev - 1);
+      updateQueryParams(
+        { frontpage: String(Number(frontpage) - 1) },
+        navigate,
+        location
+      );
     }
-  }
+  };
 
-  function handleNextClick() {
+  const handleNextClick = () => {
     if (pageNum < calculateTotalPages(pokemonList.length)) {
       setPageNum((prev) => prev + 1);
+      updateQueryParams(
+        { frontpage: String(Number(frontpage) + 1) },
+        navigate,
+        location
+      );
     }
-  }
+  };
+
+  useEffect(() => {
+    if (frontpage) {
+      if (Number(frontpage) > calculateTotalPages(pokemonList.length)) {
+        frontpage = String(calculateTotalPages(pokemonList.length));
+      }
+      setPageNum(Number(frontpage));
+      updateQueryParams({ frontpage: frontpage }, navigate, location);
+    }
+  }, []);
 
   return (
     <div className={style.wrapper}>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,21 @@
+import { NavigateFunction, Location } from 'react-router-dom';
+
+type QueryParams = {
+  [key: string]: string;
+};
+
+export const updateQueryParams = (
+  newParams: QueryParams,
+  navigate: NavigateFunction,
+  location: Location
+) => {
+  const updatedSearchParams = new URLSearchParams(location.search);
+  Object.keys(newParams).forEach((key) => {
+    updatedSearchParams.set(key, newParams[key]);
+  });
+  navigate({ search: updatedSearchParams.toString() }, { replace: true });
+};
+
 export function firstLetterUppercase(word: string): string {
   if (!word) {
     return '';
