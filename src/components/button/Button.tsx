76f31740a47306084a3243758ff2d92b86cfd1fd
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-07-13 08:38:26 +0300

    feat: finished Pokemon Card component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,17 +2,20 @@ import style from './button.module.scss';
 
 export enum ButtonType {
   GREEN = 'greenButton',
+  RED = 'redButton',
 }
 
 type ButtonProps = {
   title: string;
   btnType: ButtonType;
-  onClick: () => void;
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  id?: string;
 };
 
-export const Button = ({ title, btnType, onClick }: ButtonProps) => {
+export const Button = ({ title, btnType, onClick, id }: ButtonProps) => {
   return (
     <button
+      id={id ?? ''}
       type="submit"
       onClick={onClick}
       className={`${style.defaultButton} ${style[btnType]}`}
--- src/components/card-section/PokemonCard.tsx
+++ src/components/card-section/PokemonCard.tsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from 'react';
 import { PokemonData } from '../../api/restApi';
 import style from './pokemonCard.module.scss';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { updateQueryParams } from '../../utils/utils';
+import { firstLetterUppercase, updateQueryParams } from '../../utils/utils';
+import { Button, ButtonType } from '../button/Button';
+import { PokemonIcon } from '../pokemon-icon/PokemonIcon';
+import ExpIco from '/exp-ico.svg';
+import AbilIco from '/abil-ico.svg';
+import ItemsIco from '/items-ico.svg';
+import SpecialIco from '/special-ico.svg';
+import StatsIco from '/stats-ico.svg';
 
 type CardSectionProps = {
   cardSelected: PokemonData | null;
@@ -24,7 +31,7 @@ export const PokemonCard = ({
     }
   }, [cardSelected]);
 
-  function handleCLick(
+  function handleClick(
     e: React.MouseEvent<HTMLDivElement | HTMLButtonElement, MouseEvent>
   ) {
     const target = e.target as HTMLElement;
@@ -39,17 +46,139 @@ export const PokemonCard = ({
 
   return (
     <div
-      onClick={handleCLick}
-      className={`${style.background} ${active ? style.active : ''}`}
+      onClick={handleClick}
+      className={`${style.background} ${active ? style.backgroundActive : ''}`}
       id="close-card-back"
     >
-      <div className={`${style.wrapper} `}>
-        This is Card Section
-        <p>Card selected: {cardSelected ? 'Yes' : 'No'}</p>
-        <p> {cardSelected ? cardSelected.name : ''}</p>
-        <button id="close-card-btn" onClick={handleCLick}>
-          Close
-        </button>
+      <div className={style.wrapper}>
+        <Button
+          id="close-card-btn"
+          title="X"
+          onClick={handleClick}
+          btnType={ButtonType.RED}
+        />
+
+        {cardSelected ? (
+          <div className={style.pokemonCardWrapper}>
+            <p className={style.pokemonCardTitle}>
+              {firstLetterUppercase(cardSelected.name)}
+            </p>
+            <div className={style.iconsWrapper}>
+              <PokemonIcon
+                width={120}
+                src={cardSelected.sprites.front_default}
+                alt="pokemon_img"
+              />
+              <PokemonIcon
+                width={120}
+                src={cardSelected.sprites.back_default}
+                alt="pokemon_img"
+              />
+            </div>
+            <p>
+              Experience: {cardSelected.base_experience}{' '}
+              <img width={12} src={ExpIco} alt="Exp" />
+            </p>
+            <h3 className={style.cardH3}>
+              Stats <img width={14} src={StatsIco} alt="Stats" /> :
+            </h3>
+            <ul className={style.cardUl}>
+              {cardSelected.stats.length > 0 ? (
+                cardSelected.stats.map((stat) => {
+                  return (
+                    <li key={stat.stat.name} className={style.cardLi}>
+                      <div className={style.statItemWrapper}>
+                        <div
+                          className={
+                            stat.stat.name === 'hp'
+                              ? style.statNameHp
+                              : style.statName
+                          }
+                        >
+                          {firstLetterUppercase(stat.stat.name)}
+                        </div>
+                        {stat.stat.name === 'hp' ? (
+                          <div className={style.statValueHp}>
+                            {`${stat.base_stat} / ${stat.base_stat}`}
+                          </div>
+                        ) : (
+                          <div className={style.statValue}>
+                            {stat.base_stat}
+                          </div>
+                        )}
+                      </div>
+                    </li>
+                  );
+                })
+              ) : (
+                <li className={style.cardLi}>Empty</li>
+              )}
+            </ul>
+            <div className={style.abilItemsWrapper}>
+              <div style={{ width: '100%' }}>
+                <h3 className={style.cardH3}>
+                  Abilities <img width={14} src={AbilIco} alt="Abil" /> :
+                </h3>
+                <ul className={style.cardUl}>
+                  {cardSelected.abilities.length > 0 ? (
+                    cardSelected.abilities.map((abil) => {
+                      return (
+                        <li key={abil.ability.name} className={style.cardLi}>
+                          {firstLetterUppercase(abil.ability.name)}
+                        </li>
+                      );
+                    })
+                  ) : (
+                    <li className={style.cardLi}>No any Abilities.</li>
+                  )}
+                </ul>
+              </div>
+              <div style={{ width: '100%' }}>
+                <h3 className={style.cardH3}>
+                  Items <img width={14} src={ItemsIco} alt="Items" /> :
+                </h3>
+                <ul className={style.cardUl}>
+                  {cardSelected.held_items.length > 0 ? (
+                    cardSelected.held_items.map((item) => {
+                      return (
+                        <li key={item.item.name} className={style.cardLi}>
+                          {firstLetterUppercase(item.item.name)}
+                        </li>
+                      );
+                    })
+                  ) : (
+                    <li className={style.cardLi}>Empty</li>
+                  )}
+                </ul>
+              </div>
+            </div>
+            <div className={style.specMovesWrapper}>
+              <h3 className={style.cardH3}>
+                Special Moves <img width={14} src={SpecialIco} alt="Special" />{' '}
+                :
+              </h3>
+              <ul className={style.cardUl}>
+                {cardSelected.moves.length > 0 ? (
+                  cardSelected.moves.map((move) => {
+                    return (
+                      <li key={move.move.name} className={style.cardLi}>
+                        {firstLetterUppercase(move.move.name)}
+                      </li>
+                    );
+                  })
+                ) : (
+                  <li className={style.cardLi}>Empty</li>
+                )}
+              </ul>
+            </div>
+            <div className={style.bodyCharatWrapper}>
+              <p>Height: {cardSelected.height}</p>
+              <p>Weight: {cardSelected.weight}</p>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
