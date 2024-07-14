03dbce522171f4f661f7b941316cb7442fdbebc9
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-07-14 10:31:03 +0300

    feat: finished test settings, added test for App.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { UserConfigExport } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig: UserConfigExport = defineConfig({
   plugins: [react()],
-})
+});
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text'],
+    },
+  },
+});
+
+export default {
+  ...viteConfig,
+  ...vitestConfig,
+};
