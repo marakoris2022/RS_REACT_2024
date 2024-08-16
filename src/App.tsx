bf269edbe2d53cbd260fe6de36696bbeb01689d7
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-08-16 17:26:04 +0300

    feat: Husky installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <p>Hello</p>
-    </>
-  );
+    return (
+        <>
+            <p>Hello</p>
+        </>
+    );
 }
 
 export default App;
